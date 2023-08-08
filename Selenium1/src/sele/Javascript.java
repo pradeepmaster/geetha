54adaa52ba89c1e4fde7d607786d69c28db5a801
pradeepmaster <135145467+pradeepmaster@users.noreply.github.com>
2023-08-08 11:27:27 +0530

    Update Javascript.java

--- Selenium1/src/sele/Javascript.java
+++ Selenium1/src/sele/Javascript.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 //pradeep changed file
+//changed from git to eclipse
 public class Javascript {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
