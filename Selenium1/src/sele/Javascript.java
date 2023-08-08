e0a6e25fc79f37709f6a4bb0eff7178ad49963a1
pradeepmaster <pradeepachari58@gmail.com>
2023-08-08 11:22:40 +0530

    javascript code edit1

--- Selenium1/src/sele/Javascript.java
+++ Selenium1/src/sele/Javascript.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//pradeep changed file
 public class Javascript {
 
 	public static void main(String[] args) throws AWTException, InterruptedException {
